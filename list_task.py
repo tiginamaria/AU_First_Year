661fd966d6dc5b04fa7d2b772f3165337ce0639d
Maria Tigina <31625351+tiginamaria@users.noreply.github.com>
2017-09-15 00:12:52 +0300

    Add files via upload

---
+++ list_task.py
@@ -0,0 +1,41 @@
+# Remove equal adjacent elements
+#
+# Example input: [1, 2, 2, 3]
+# Example output: [1, 2, 3]
+
+a = [int(s) for s in input().split()]
+def remove_adjacent(a):
+    b = []
+    for i in a:
+        if i not in b:
+            b.append(i)
+    return (b)
+b=remove_adjacent(a)
+print(' '.join([str(i) for i in b]))
+
+# Merge two sorted lists in one sorted list in linear time
+#
+# Example input: [2, 4, 6], [1, 3, 5]
+# Example output: [1, 2, 3, 4, 5, 6]
+x = [int(s) for s in input().split()]
+y = [int(s) for s in input().split()]
+def linear_merge(a,b):
+    i=0
+    j=0
+    c=[]
+    while (i < len(a)) or (j < len(b)):
+            if j>=len(b):
+                c.append(a[i])
+                i=i+1
+            elif i>= len(a):
+                c.append(b[j])
+                j=j+1
+            elif a[i]<b[j]:
+                c.append(a[i])
+                i=i+1
+            else:
+                c.append(b[j])
+                j+=1
+    return (c)
+c=linear_merge(x,y)
+print(' '.join([str(i) for i in c]))
---
+++ string_task.py
@@ -0,0 +1,67 @@
+# Given a string, if its length is at least 3,
+# add 'ing' to its end.
+# Unless it already ends in 'ing', in which case
+# add 'ly' instead.
+# If the string length is less than 3, leave it unchanged.
+# Return the resulting string.
+#
+# Example input: 'read'
+# Example output: 'reading'
+
+a=input()
+def verbing(s):
+    if len(s)>=3:  
+        if s[-3: len(s)]=='ing':
+            return(s+'ly')
+        else:
+            return(s+"ing")
+    else:
+        return(s)
+print(verbing(a))
+
+
+# Given a string, find the first appearance of the
+# substring 'not' and 'bad'. If the 'bad' follows
+# the 'not', replace the whole 'not'...'bad' substring
+# with 'good'.
+# Return the resulting string.
+#
+# Example input: 'This dinner is not that bad!'
+# Example output: 'This dinner is good!'
+a=input()
+def not_bad(s):
+    a=s.find('not')
+    b=s.find('bad',a)
+    c=""
+    if a!=-1 and b!=-1:
+            c=s[0:a]+"good"+s[b+3:len(s)]
+            return(c)
+    else:
+            return (s)
+print(not_bad(a))
+
+# Consider dividing a string into two halves.
+# If the length is even, the front and back halves are the same length.
+# If the length is odd, we'll say that the extra char goes in the front half.
+# e.g. 'abcde', the front half is 'abc', the back half 'de'.
+#
+# Given 2 strings, a and b, return a string of the form
+#  a-front + b-front + a-back + b-back
+#
+# Example input: 'abcd', 'xy'
+# Example output: 'abxcdy'
+a=input()
+b=input()
+def front_back(x, y):
+    xf=""
+    xb=""
+    yf=""
+    yb=""
+    c=""
+    xf=x[:(len(x)+1)//2]
+    xb=x[len(xf):]
+    yf=y[:(len(y)+1)//2]
+    yb=y[len(yf):]
+    c=xf+yf+xb+yb
+    return(c)
+print(front_back(a,b))
