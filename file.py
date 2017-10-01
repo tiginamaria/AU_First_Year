365abad6700312e69c3139df14188377045a4a17
Maria Tigina <31625351+tiginamaria@users.noreply.github.com>
2017-10-01 22:31:24 +0300

    Update file.py

--- file.py
+++ file.py
@@ -16,18 +16,18 @@ def hash_file(path):
 
 
 def find_duplicates(parent):
-    hash_to_file = defaultdict(list)
+    file_to_hash = defaultdict(list)
     for root, _, files in os.walk(parent):
         for f_name in files:
             path = os.path.join(root, f_name)
             if not f_name.startswith(('.', '~')) and not os.path.islink(path):
-                hashf = hash_file(path)
-                hash_to_file[hashf].append(os.path.relpath(path, start=parent))
-    return hash_to_file
+                hashf = file_to_hash(path)
+                file_to_hash[hashf].append(os.path.relpath(path, start=parent))
+    return file_to_hash
 
 
-def print_files(hash_to_file):
-    for files in hash_to_file.values():
+def print_files(file_to_hash):
+    for files in file_to_hash.values():
         if len(files) > 1:
             print(':'.join(files))
 
