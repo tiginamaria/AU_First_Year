cbad6835499b80c2722912df306bfe645be2deb3
Maria Tigina <31625351+tiginamaria@users.noreply.github.com>
2017-12-09 12:57:11 +0300

    Update Tree.hs

--- Tree.hs
+++ Tree.hs
@@ -5,14 +5,14 @@ data BinaryTree k v = Leaf | Branch k v (BinaryTree k v) (BinaryTree k v) derivi
 lookup :: Ord k => k -> BinaryTree k v -> Maybe v
 lookup _ Leaf = Nothing
 lookup key (Branch k v l r) | key == k  = Just v
-							| key > k   = lookup key r
-							| otherwise = lookup key l
+			    | key > k   = lookup key r
+			    | otherwise = lookup key l
 
 insert :: Ord k => k -> v -> BinaryTree k v -> BinaryTree k v
 insert key value Leaf = Branch key value Leaf Leaf
 insert key value (Branch k v l r) | key == k  = Branch key value l r
-								  | key > k   = Branch k v l (insert key value r)
-							      | otherwise = Branch k v (insert key value l) r
+				  | key > k   = Branch k v l (insert key value r)
+				  | otherwise = Branch k v (insert key value l) r
 
 merge :: BinaryTree k v -> BinaryTree k v -> BinaryTree k v
 merge b Leaf = b
@@ -21,7 +21,6 @@ merge b (Branch k v l r) = Branch k v (merge b l) r
 delete :: Ord k => k -> BinaryTree k v -> BinaryTree k v
 delete _ Leaf = Leaf
 delete key (Branch k v r l) | key == k  = merge l r
-						    | key > k   = Branch k v l (delete key r)
-							| otherwise = Branch k v (delete key l) r
+			    | key > k   = Branch k v l (delete key r)
+			    | otherwise = Branch k v (delete key l) r
 							
-
