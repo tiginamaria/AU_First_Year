d30f288ff5a1a656a419a2bdb80b837c90642c09
Maria Tigina <31625351+tiginamaria@users.noreply.github.com>
2017-12-09 22:49:32 +0300

    Update Tree.hs

--- HW8/Tree.hs
+++ HW8/Tree.hs
@@ -4,24 +4,25 @@ data BinaryTree k v = Leaf | Branch k v (BinaryTree k v) (BinaryTree k v) derivi
 
 lookup :: Ord k => k -> BinaryTree k v -> Maybe v
 lookup _ Leaf = Nothing
-lookup key (Branch k v l r) | key == k  = Just v
-			    | key > k   = lookup key r
-			    | key < k   = lookup key l
+lookup key (Branch k v l r) 
+                           | key > k    = lookup key r
+                           | key < k    = lookup key l
+                           | otherwise  = Just v
 
 insert :: Ord k => k -> v -> BinaryTree k v -> BinaryTree k v
 insert key value Leaf = Branch key value Leaf Leaf
-insert key value (Branch k v l r) | key == k  = Branch key value l r
-				  | key > k   = Branch k v l (insert key value r)
-				  | key < k   = Branch k v (insert key value l) r
+insert key value (Branch k v l r) 
+                                 | key > k    = Branch k v l (insert key value r)
+                                 | key < k    = Branch k v (insert key value l) r
+                                 | otherwise  = Branch key value l r
 
-merge :: BinaryTree k v -> BinaryTree k v -> BinaryTree k v
+merge :: Ord k => BinaryTree k v -> BinaryTree k v -> BinaryTree k v
 merge b Leaf = b
-merge Leaf b = b
-merge (Branch k v l r) b = Branch k v (merge l b) r
+merge b (Branch k v l r) = Branch k v (merge b l) r
 
 delete :: Ord k => k -> BinaryTree k v -> BinaryTree k v
 delete _ Leaf = Leaf
-delete key (Branch k v r l) | key == k  = merge l r
-			    | key > k   = Branch k v l (delete key r)
-			    | key < k   = Branch k v (delete key l) r
-							
+delete key (Branch k v r l) 
+                            | key > k    = Branch k v l (delete key r)
+                            | key < k    = Branch k v (delete key l) r
+                            | otherwise  = merge l r
