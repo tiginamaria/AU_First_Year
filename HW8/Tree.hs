6e1a7787eba78c9162cdcdbc9085858f0dec7c89
Maria Tigina <31625351+tiginamaria@users.noreply.github.com>
2017-12-09 21:01:37 +0300

    Update Tree.hs

--- HW8/Tree.hs
+++ HW8/Tree.hs
@@ -6,21 +6,22 @@ lookup :: Ord k => k -> BinaryTree k v -> Maybe v
 lookup _ Leaf = Nothing
 lookup key (Branch k v l r) | key == k  = Just v
 			    | key > k   = lookup key r
-			    | otherwise = lookup key l
+			    | key < k   = lookup key l
 
 insert :: Ord k => k -> v -> BinaryTree k v -> BinaryTree k v
 insert key value Leaf = Branch key value Leaf Leaf
 insert key value (Branch k v l r) | key == k  = Branch key value l r
 				  | key > k   = Branch k v l (insert key value r)
-				  | otherwise = Branch k v (insert key value l) r
+				  | key < k   = Branch k v (insert key value l) r
 
 merge :: BinaryTree k v -> BinaryTree k v -> BinaryTree k v
 merge b Leaf = b
-merge b (Branch k v l r) = Branch k v (merge b l) r
+merge Leaf b = b
+merge (Branch k v l r) b = Branch k v (merge l b) r
 
 delete :: Ord k => k -> BinaryTree k v -> BinaryTree k v
 delete _ Leaf = Leaf
 delete key (Branch k v r l) | key == k  = merge l r
 			    | key > k   = Branch k v l (delete key r)
-			    | otherwise = Branch k v (delete key l) r
+			    | key < k   = Branch k v (delete key l) r
 							
