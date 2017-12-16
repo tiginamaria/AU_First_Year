cb97f1d77c1fe34727a7b8fcfce81cc585a412b4
Maria Tigina <31625351+tiginamaria@users.noreply.github.com>
2017-12-16 09:20:33 +0300

    Update 5.sql

--- sql/5.sql
+++ sql/5.sql
@@ -1,3 +1,5 @@
 SELECT GovernmentForm, SUM(SurfaceArea) AS SumSurfaseArea
 FROM Country
-GROUP BY GovernmentForm ORDER BY SumSurfaseArea DESC LIMIT 1;
+GROUP BY GovernmentForm 
+ORDER BY SumSurfaseArea DESC 
+LIMIT 1;
