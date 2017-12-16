e1d0aab1ed664079b6018a3569a28c02d50815b7
Maria Tigina <31625351+tiginamaria@users.noreply.github.com>
2017-12-16 07:58:11 +0300

    Update 6.sql

--- sql/6.sql
+++ sql/6.sql
@@ -1,3 +1,4 @@
-SELECT City.Name, City.Population AS CityPopulation, Country.Population AS CountryPopulation FROM City, Country
+SELECT City.Name, City.Population AS CityPopulation, Country.Population AS CountryPopulation 
+FROM City, Country
 WHERE Country.Population > 0 AND City.CountryCode = Country.Code 
 ORDER BY (CityPopulation / CountryPopulation) DESC, City.Name DESC LIMIT 20;
