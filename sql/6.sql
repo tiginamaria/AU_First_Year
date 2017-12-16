1f8def31f9afd502346fb8326db83d45ab3474db
Maria Tigina <31625351+tiginamaria@users.noreply.github.com>
2017-12-16 09:31:26 +0300

    Update 6.sql

--- sql/6.sql
+++ sql/6.sql
@@ -1,4 +1,5 @@
 SELECT City.Name, City.Population AS CityPopulation, Country.Population AS CountryPopulation 
-FROM City, Country
-WHERE Country.Population > 0 AND City.CountryCode = Country.Code 
-ORDER BY (CityPopulation / CountryPopulation) DESC, City.Name DESC LIMIT 20;
+FROM City
+JOIN Country ON Country.Population > 0 AND City.CountryCode = Country.Code 
+ORDER BY CAST(CityPopulation AS FLOAT) / CAST (CountryPopulation AS FLOAT) DESC, City.Name DESC 
+LIMIT 20;
